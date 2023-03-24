e43f5ee3f2f814676c250145d56920476d24b2c9
Adrian <Adrian>
2023-03-24 17:12:57 +0100

    add strapi test

--- tests/strapi/test_strapi.py
+++ tests/strapi/test_strapi.py
@@ -1,5 +1,18 @@
 from pipelines.strapi import strapi_source
 
+import dlt
+import pytest
+from tests.utils import ALL_DESTINATIONS, assert_load_info
+
+
+
+@pytest.mark.parametrize('destination_name', ALL_DESTINATIONS)
+def test_all_resources(destination_name: str) -> None:
+    pipeline = dlt.pipeline(pipeline_name='strapi', destination=destination_name, dataset_name='strapi', full_refresh=True)
+    load_info = pipeline.run(strapi_source())
+    print(load_info)
+    assert_load_info(load_info)
+
 def test_strapi_source():
     endpoints = ['athletes']
     resources = strapi_source(endpoints=endpoints).resources.keys()
