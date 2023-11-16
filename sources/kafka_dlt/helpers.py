2364c2c602265f5c697cbb2fbd993343ee10f889
Josué Sehnem <josue@sehnem.com>
2023-11-16 18:31:20 -0300

    wip

--- sources/kafka_dlt/helpers.py
+++ sources/kafka_dlt/helpers.py
@@ -1,10 +1,11 @@
 """Helpers for the Kafka DLT source."""
 
 import dlt
+import json
 from typing import Optional
 
 # from kafka import KafkaConsumer
-from confluent_kafka import Consumer
+from confluent_kafka import Consumer, TopicPartition, OFFSET_BEGINNING
 from dlt.common.configuration.specs import BaseConfiguration, configspec
 
 
@@ -36,3 +37,31 @@ def consumer_from_credentials(
         'sasl.password': sasl_password
     }
     return Consumer(config)
+
+
+def get_topic_partitions(
+        consumer: Consumer,
+        topic: str,
+        partitions: int,
+        parse_json: bool = False,
+        incremental: Optional[dlt.sources.incremental] = None,
+):
+    """Get all documents from a kafka topic."""
+
+    if incremental is None:
+        time_ago = OFFSET_BEGINNING
+    else:
+        time_ago = incremental.last_value
+
+    topic_partitions = []
+    for partition in range(partitions):
+        topic_partitions.append(TopicPartition(topic, partition, time_ago))
+    consumer.assign(topic_partitions)
+
+    for msg in consumer.consume():
+        if msg is None:
+            continue
+        if parse_json:
+            yield json.loads(msg.value())
+        else:
+            yield msg.value()
